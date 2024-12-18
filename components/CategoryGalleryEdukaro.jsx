a1f6309d1a3005b4c67518e0f20d6e2beeec871d
Praveen <praveennegi700@gmail.com>
2024-12-18 12:00:47 +0530

    images

--- components/CategoryGalleryEdukaro.jsx
+++ components/CategoryGalleryEdukaro.jsx
@@ -24,7 +24,7 @@ const CategoryGalleryEdukaro = ({ data, index, citySlug }) => {
               width={1000}
               height={1000}
               className="object-cover w-full h-full"
-              src={`https://res.cloudinary.com/eduminatti-com/image/upload/v1729853967/Go%20Edu/${citySlug}/${
+              src={`https://res.cloudinary.com/eduminatti-com/image/upload/v1733290846/EduKaro/${citySlug}/${
                 ['G', 'I', 'H', 'G'][idx]
               }-${data?.fields?.Image_Code}.png`}
               alt={`Category ${cardId}`}
