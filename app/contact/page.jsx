a372b004e9c98b64cf113c24385705a6b6d17f37
Praveen <praveennegi700@gmail.com>
2025-01-14 11:50:27 +0530

    gmail add in edukaro

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -47,7 +47,7 @@ const Contact = () => {
                 </li>
                 <li className="flex items-center gap-3">
                   <SlEnvolope className="h-8 w-5 " />
-                  <div>info@edukaro.com</div>
+                  <div>edukaroindia@gmail.com</div>
                 </li>
                
 
